919ed9d0fb832d4aac23887b81b59c988b2788b3
cass1618 <cass16180@gmail.com>
2021-05-17 13:48:04 -0700

    :white_check_mark: Pass test for GetAll with Database

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -38,4 +38,8 @@ namespace ToDoList
       });
     }
   }
+  public static class DBConfiguration
+  {
+    public static string ConnectionString = "server=localhost;user id=root;password=Luna4444;port=3306;database=to_do_list;";
+  }
 }
