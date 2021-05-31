376d1d5fe9b19cd4f23452713d5c375595de7458
cass1618 <cass16180@gmail.com>
2021-05-31 11:25:59 -0700

    Setup many-to-many database migrations

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,9 +1,17 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
-    public int ItemId { get; set; }
-    public string Description { get; set; }
-    public int Done { get; set; } = 0;
+    public Item()
+        {
+            this.JoinEntities = new HashSet<CategoryItem>();
+        }
+
+        public int ItemId { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<CategoryItem> JoinEntities { get;}
   }
 }
